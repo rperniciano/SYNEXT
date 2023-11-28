4179a075700c2ff25f534b3ae737bce5e0300850
CianoWork <ricki.perniciano.work@gmail.com>
2023-11-28 19:57:47 +0100

    feat: changing component

--- components/header/menu/logo.tsx
+++ components/header/menu/logo.tsx
@@ -1,12 +1,12 @@
-import Logo from "./image/logo.png"
+import Logo from "./image/logo.png";
 import Image from "next/image";
 
-const MenusLogo:React.FC=():React.ReactElement=>{
-    return(
-        <div className="header__logo-box">
-            <Image src={Logo} alt="Logo" className="header__logo"/>
-        </div>
-    );
+const MenusLogo: React.FC = (): React.ReactElement => {
+  return (
+    <div className="header__logo-box">
+      <Image src={Logo} alt="Logo" className="header__logo" />
+    </div>
+  );
 };
 
 export default MenusLogo;
