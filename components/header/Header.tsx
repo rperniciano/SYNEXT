cb4e37f59c889fd4b18eb6e25f055fe362f3563a
CianoWork <ricki.perniciano.work@gmail.com>
2023-11-28 18:58:49 +0100

    feat: changing component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,12 @@
 import HeaderText from "./header-text/header-text";
 import IntroVideo from "./hero/IntroVideo";
+import MenusLogo from "./menu/logo";
 
 const Header = () => {
   return (
     <>
       <header id="header" className="header">
+        <MenusLogo></MenusLogo>
         <HeaderText></HeaderText>
         <IntroVideo></IntroVideo>
       </header>
