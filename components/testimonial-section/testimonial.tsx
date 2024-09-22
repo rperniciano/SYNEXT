0ce584b46e183f172c5f5d2f08a22b56acc5e69c
CianoWork <ricki.perniciano.work@gmail.com>
2024-09-22 22:34:27 +0200

    feat: modifying foto

--- components/testimonial-section/testimonial.tsx
+++ components/testimonial-section/testimonial.tsx
@@ -4,6 +4,7 @@ import Heading from "./private/heading";
 import Immagine1 from "./image/automatizzainvestimenti.jpg";
 import Immagine2 from "./image/communityglobale.jpg";
 import Immagine3 from "./image/costruisciunimpero.jpg";
+import Immagine4 from "./image/foto curriculum.jpg";
 
 const Testimonials = () => {
   return (
@@ -11,16 +12,16 @@ const Testimonials = () => {
         <Heading></Heading>
         <div className="row row__flex u-margin-inline-big">
             <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Luca Floris, 18">
-                <Image src={Immagine1} alt="" className="testimonial-card__image"></Image>
+                <Image src={Immagine1} alt="Luca Floris" className="testimonial-card__image"></Image>
             </Card>
-            <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Michele Deiana, 22">
-                <Image src={Immagine2} alt="" className="testimonial-card__image"></Image>
+            <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Michele Deiana, 21">
+                <Image src={Immagine2} alt="Michele Deiana" className="testimonial-card__image"></Image>
             </Card>
             <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Riccardo Perniciano, 25">
-                <Image src={Immagine3} alt="" className="testimonial-card__image"></Image>
+                <Image src={Immagine4} alt="Riccardo Perniciano" className="testimonial-card__image"></Image>
             </Card>
             <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Matteo Presotto, 23">
-                <Image src={Immagine3} alt="" className="testimonial-card__image"></Image>
+                <Image src={Immagine3} alt="Matteo Presotto" className="testimonial-card__image"></Image>
             </Card>
         </div>
     </section>
