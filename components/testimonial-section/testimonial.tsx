f065b09f0aa0ac3090b75ba6cbaaa8fe4981acb5
CianoWork <ricki.perniciano.work@gmail.com>
2024-09-22 22:38:39 +0200

    feat: lint and adding border on image

--- components/testimonial-section/testimonial.tsx
+++ components/testimonial-section/testimonial.tsx
@@ -9,23 +9,51 @@ import Immagine4 from "./image/foto curriculum.jpg";
 const Testimonials = () => {
   return (
     <section className="section-testimonials">
-        <Heading></Heading>
-        <div className="row row__flex u-margin-inline-big">
-            <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Luca Floris, 18">
-                <Image src={Immagine1} alt="Luca Floris" className="testimonial-card__image"></Image>
-            </Card>
-            <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Michele Deiana, 21">
-                <Image src={Immagine2} alt="Michele Deiana" className="testimonial-card__image"></Image>
-            </Card>
-            <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Riccardo Perniciano, 25">
-                <Image src={Immagine4} alt="Riccardo Perniciano" className="testimonial-card__image"></Image>
-            </Card>
-            <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Matteo Presotto, 23">
-                <Image src={Immagine3} alt="Matteo Presotto" className="testimonial-card__image"></Image>
-            </Card>
-        </div>
+      <Heading></Heading>
+      <div className="row row__flex u-margin-inline-big">
+        <Card
+          paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services."
+          author="Luca Floris, 18"
+        >
+          <Image
+            src={Immagine1}
+            alt="Luca Floris"
+            className="testimonial-card__image"
+          ></Image>
+        </Card>
+        <Card
+          paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services."
+          author="Michele Deiana, 21"
+        >
+          <Image
+            src={Immagine2}
+            alt="Michele Deiana"
+            className="testimonial-card__image"
+          ></Image>
+        </Card>
+        <Card
+          paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services."
+          author="Riccardo Perniciano, 25"
+        >
+          <Image
+            src={Immagine4}
+            alt="Riccardo Perniciano"
+            className="testimonial-card__image"
+          ></Image>
+        </Card>
+        <Card
+          paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services."
+          author="Matteo Presotto, 23"
+        >
+          <Image
+            src={Immagine3}
+            alt="Matteo Presotto"
+            className="testimonial-card__image"
+          ></Image>
+        </Card>
+      </div>
     </section>
   );
-}
+};
 
 export default Testimonials;
