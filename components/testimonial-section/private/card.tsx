22b8eecaf94ed50cd46248d3f218f535e70c7e24
CianoWork <ricki.perniciano.work@gmail.com>
2024-09-22 20:12:02 +0200

    feat: fixing testimonials

--- components/testimonial-section/private/card.tsx
+++ components/testimonial-section/private/card.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Card = ({ paragraph, children, author }: Props) => {
   return (
-    <div className="testimonial-card u-margin-inline-big u-margin-top-big">
+    <div className="testimonial-card u-margin-inline-small u-margin-top-big">
       {children}
       <p className="testimonial-text">{paragraph}</p>
       <p className="testimonial-author">{author}</p>
--- components/testimonial-section/testimonial.tsx
+++ components/testimonial-section/testimonial.tsx
@@ -9,7 +9,7 @@ const Testimonials = () => {
   return (
     <section className="section-testimonials">
         <Heading></Heading>
-        <div className="row row__flex u-margin-inline-small">
+        <div className="row row__flex u-margin-inline-big">
             <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Luca Floris, 18">
                 <Image src={Immagine1} alt="" className="testimonial-card__image"></Image>
             </Card>
@@ -17,6 +17,9 @@ const Testimonials = () => {
                 <Image src={Immagine2} alt="" className="testimonial-card__image"></Image>
             </Card>
             <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Riccardo Perniciano, 25">
+                <Image src={Immagine3} alt="" className="testimonial-card__image"></Image>
+            </Card>
+            <Card paragraph="Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services." author="Matteo Presotto, 23">
                 <Image src={Immagine3} alt="" className="testimonial-card__image"></Image>
             </Card>
         </div>
