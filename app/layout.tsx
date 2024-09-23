91ffbc7435c76cd0d73aa2acede9b53d37f173b0
CianoWork <ricki.perniciano.work@gmail.com>
2024-09-23 14:41:59 +0200

    feat: changing logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"/>
+        <link rel="icon" type="image/png" sizes="192x192" href="https://ionevision.com/lightLogo?size=192x192"></link>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
