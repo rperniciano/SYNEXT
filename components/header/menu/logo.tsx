000f40527323544a3bfd23ba1c39c47bfb6a2eb2
CianoWork <ricki.perniciano.work@gmail.com>
2024-09-23 14:28:26 +0200

    feat: adding logo

--- components/header/menu/logo.tsx
+++ components/header/menu/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const MenusLogo: React.FC = (): React.ReactElement => {
   return (
     <div className="header__logo-box">
-      <Image src={Logo} alt="Logo" className="header__logo" />
+      <Image src="https://ionevision.com/svg/onevision-white-long.svg" width={50} height={50} alt="Logo" className="header__logo" />
     </div>
   );
 };
